dd4d95f6b212a707eecfe797070d756a13605483
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-22 16:08:59 +0530

    using for loop to make 6 turtels which can cut few lines of code

--- main.py
+++ main.py
@@ -4,36 +4,13 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_input = screen.textinput(title="make a bet", prompt="which turtle gonna win the race? type the color: ")
 color = ["red", "green", "blue", "yellow", "purple", "orange"]
+y_positions = [-100, -70, -40, -10, 20, 50]
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.color("red")
-tim.goto(x=-230, y=-100)
+for turtle_indexes in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(color[turtle_indexes])
+    tim.goto(x=-230, y=y_positions[turtle_indexes])
 
-tom = Turtle(shape="turtle")
-tom.penup()
-tom.color("green")
-tom.goto(x=-230, y=-70)
-
-jim = Turtle(shape="turtle")
-jim.penup()
-jim.color("blue")
-jim.goto(x=-230, y=-40)
-
-jam = Turtle(shape="turtle")
-jam.penup()
-jam.color("yellow")
-jam.goto(x=-230, y=-10)
-
-
-tad = Turtle(shape="turtle")
-tad.penup()
-tad.color("purple")
-tad.goto(x=-230, y=20)
-
-tad = Turtle(shape="turtle")
-tad.penup()
-tad.color("orange")
-tad.goto(x=-230, y=50)
 
 screen.exitonclick()
