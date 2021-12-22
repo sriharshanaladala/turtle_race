c7cc8164f009fe0f3e3377eeb019c1e5ca5dd308
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-22 16:08:59 +0530

    final race of turtles starts

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
+race_start = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_input = screen.textinput(title="make a bet", prompt="which turtle gonna win the race? type the color: ")
 color = ["red", "green", "blue", "yellow", "purple", "orange"]
+y_positions = [-100, -70, -40, -10, 20, 50]
+participants = []
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.color("red")
-tim.goto(x=-230, y=-100)
+for turtle_indexes in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(color[turtle_indexes])
+    new_turtle.goto(x=-230, y=y_positions[turtle_indexes])
+    participants.append(new_turtle)
 
-tom = Turtle(shape="turtle")
-tom.penup()
-tom.color("green")
-tom.goto(x=-230, y=-70)
-
-jim = Turtle(shape="turtle")
-jim.penup()
-jim.color("blue")
-jim.goto(x=-230, y=-40)
-
-jam = Turtle(shape="turtle")
-jam.penup()
-jam.color("yellow")
-jam.goto(x=-230, y=-10)
+if user_input:
+    race_start = True
 
 
-tad = Turtle(shape="turtle")
-tad.penup()
-tad.color("purple")
-tad.goto(x=-230, y=20)
+while race_start:
 
-tad = Turtle(shape="turtle")
-tad.penup()
-tad.color("orange")
-tad.goto(x=-230, y=50)
+    for turtle in participants:
+        if turtle.xcor() > 230:
+            race_start = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_input:
+                print(f"you've won,{winning_color} is the winner of this race")
+            else:
+                print(f"you've lost,{winning_color} is the winner of this race")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
